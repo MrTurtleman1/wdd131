e7ce015b56ee790e384596f07f90c2c8b36f78ff
Maxwell Tippetts <104464940+MrTurtleman1@users.noreply.github.com>
2026-05-02 16:55:22 -0700

    dom

---
+++ ponder/dom.js
@@ -0,0 +1,29 @@
+const title = document.querySelector('h1');
+
+console.log('title');
+
+///document.querySelector('#topics').style.color = 'red';
+
+//document.getElementById('topics').style.color = 'purple';
+
+let list = document.querySelector('.list')
+
+list.style.bordercolor = '3px solid black';
+
+ let para = document.querySelector('p');
+
+ para.style.backgroundColor = 'lightblue'
+
+ para.classList.add('background');
+
+// document.querySelector('body').classList.add('background');
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'img/New Piskel-1.png.png');
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
