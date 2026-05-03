fcbcfa5c64cc03c384088fa75de9963839cb10b9
Maxwell Tippetts <104464940+MrTurtleman1@users.noreply.github.com>
2026-05-02 19:29:42 -0700

    theme-finished

--- mission/mission.js
+++ mission/mission.js
@@ -1,17 +1,18 @@
 
 
-let selectElem = document.querySelector('#theme-select');
+let selectElem = document.querySelector('#webdevlist');
 let pageContent = document.querySelector('body');
+let image = document.querySelector('img');
 
-selectElem.addEventListener('change', changeTheme);
+selectElem.addEventListener('change', changeTheme)
 
 function changeTheme() {
     let current = selectElem.value;
     if (current === 'dark') {
         pageContent.classList.add('dark');
+        image.setAttribute('src', 'byui-logo-white.png')
     } else {
-        // default
-        document.body.style.backgroundImage = "none";
-        pageContent.style.fontFamily = "Georgia, serif";
+        pageContent.classList.remove('dark');
+        image.setAttribute('src', 'byui-logo-blue.webp')
     }
 }
